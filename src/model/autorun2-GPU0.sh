844d40f875f9147afa2c0723a8f5b3e763d0fa74
Nigiva <corentin.duchene@epita.fr>
2022-03-31 01:22:39 +0200

    feat: autorun2 and rename autorun1

---
+++ src/model/autorun2-GPU0.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/work2/home/ing1/corentin/hatespeech-detection-models/venv/bin/python /work2/home/ing1/corentin/hatespeech-detection-models/src/model/py/glove-cct-bce.py
---
+++ src/model/autorun2-GPU1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/work2/home/ing1/corentin/hatespeech-detection-models/venv/bin/python /work2/home/ing1/corentin/hatespeech-detection-models/src/model/py/fasttext-resnet44-bce.py
+/work2/home/ing1/corentin/hatespeech-detection-models/venv/bin/python /work2/home/ing1/corentin/hatespeech-detection-models/src/model/py/glove-resnet32-bce.py
+/work2/home/ing1/corentin/hatespeech-detection-models/venv/bin/python /work2/home/ing1/corentin/hatespeech-detection-models/src/model/py/glove-resnet44-bce.py
+/work2/home/ing1/corentin/hatespeech-detection-models/venv/bin/python /work2/home/ing1/corentin/hatespeech-detection-models/src/model/py/glove-resnet44-pwbce.py
+/work2/home/ing1/corentin/hatespeech-detection-models/venv/bin/python /work2/home/ing1/corentin/hatespeech-detection-models/src/model/py/robertaemb-resnet44-bce.py
+/work2/home/ing1/corentin/hatespeech-detection-models/venv/bin/python /work2/home/ing1/corentin/hatespeech-detection-models/src/model/py/unfreeze-glove-resnet44-bce.py
